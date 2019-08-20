15d8d65298a757376ccd8ba7931cf302966a9532
Louis Taing <louistaing@Anands-MBP.guest.corp.microsoft.com>
2019-08-20 10:40:35 -0700

    Get Started Done

--- ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
 
 namespace ContosoUniversity.Models
 {
@@ -14,5 +15,7 @@ namespace ContosoUniversity.Models
         }
 
         public DbSet<ContosoUniversity.Models.Student> Student { get; set; }
+        public DbSet<ContosoUniversity.Models.Course> Course { get; set; }
+        public DbSet<ContosoUniversity.Models.Enrollment> Enrollment { get; set; }
     }
 }
--- ContosoUniversity/ContosoUniversity/Program.cs
+++ ContosoUniversity/ContosoUniversity/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;   // CreateScope
 using Microsoft.Extensions.Logging;
 using System;
+//using ContosoUniversity.Data; Vurrent bug doesn't use
 
 namespace ContosoUniversity
 {
@@ -21,6 +22,7 @@ namespace ContosoUniversity
                 {
                     var context = services.GetRequiredService<SchoolContext>();
                     context.Database.EnsureCreated();
+                    DbInitializer.Initialize(context);
                 }
                 catch (Exception ex)
                 {
